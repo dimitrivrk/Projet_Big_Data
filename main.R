d3a8d86896f2d7c1cac8b22b18bc1b17423cc6a2
dimitrivrk <dimitri.varakine@outlook.fr>
2024-06-10 13:42:08 +0200

    Conversion

--- main.R
+++ main.R
@@ -4,34 +4,26 @@ data <- data.frame(data)
 
 # pour chaque colonne modifier l'encodage  passe les en UTF8 car il ne le sont pas de base
 for (i in 1:ncol(data)) {
-  if (is.character(data[,i])) {
     data[,i] <- iconv(data[,i], to = "UTF-8")
-  }
 }
 
 # Description du jeu de données
-summary(data)
+#summary(data)
 
-# Affichage des premières lignes du jeu de données
-#head(data)
+head(data)
 
 
-# met tous les caractères en miniscule
-for (i in 1:ncol(data)) {
-    data[,i] <- tolower(data[,i])
-  
-}
 
 # Conversion des types de données
-data$X <- as.factor(data$X)
-data$Y <- as.factor(data$Y)
-data$OBJECTID <- as.factor(data$OBJECTID)
+data$X <- as.numeric(data$X)
+data$Y <- as.numeric(data$Y)
+data$OBJECTID <- as.numeric(data$OBJECTID)
 data$created_date <- as.Date(data$created_date, format = "%Y/%m/%d %H:%M:%S%z")
 data$created_user <- as.factor(data$created_user)
 data$src_geo <- as.factor(data$src_geo)
 data$clc_quartier <- as.factor(data$clc_quartier)
 data$clc_secteur <- as.factor(data$clc_secteur)
-data$id_arbre <- as.factor(data$id_arbre)
+data$id_arbre <- as.numeric(data$id_arbre)
 data$haut_tot <- as.numeric(data$haut_tot)
 data$haut_tronc <- as.numeric(data$haut_tronc)
 data$tronc_diam <- as.numeric(data$tronc_diam)
@@ -40,32 +32,44 @@ data$fk_stadedev <- as.factor(data$fk_stadedev)
 data$fk_port <- as.factor(data$fk_port)
 data$fk_pied <- as.factor(data$fk_pied)
 data$fk_situation <- as.factor(data$fk_situation)
-# data$fk_revetement <- as.factor(data$fk_revetement)
-data$commentaire_environnement <- as.factor(data$commentaire_environnement)
+
+data$fk_revetement <- data$fk_revetement == "Oui"
+
+data$commentaire_environnement <- as.character(data$commentaire_environnement)
 data$dte_plantation <- as.Date(data$dte_plantation, format = "%Y/%m/%d %H:%M:%S%z")
 data$age_estim <- as.numeric(data$age_estim)
-data$fk_prec_estim <- as.factor(data$fk_prec_estim)
-data$clc_nbr_diag <- as.factor(data$clc_nbr_diag)
+data$fk_prec_estim <- as.numeric(data$fk_prec_estim)
+data$clc_nbr_diag <- as.numeric(data$clc_nbr_diag)
 data$dte_abattage <- as.Date(data$dte_abattage, format = "%Y/%m/%d %H:%M:%S%z")
-data$fk_nomtech <- as.factor(data$fk_nomtech)
+data$fk_nomtech <- as.character(data$fk_nomtech)
 data$last_edited_user <- as.factor(data$last_edited_user)
 data$last_edited_date <- as.Date(data$last_edited_date, format = "%Y/%m/%d %H:%M:%S%z")
 data$villeca <- as.factor(data$villeca)
-data$nomfrancais <- as.factor(data$nomfrancais)
-data$nomlatin <- as.factor(data$nomlatin)
-data$GlobalID <- as.factor(data$GlobalID)
+data$nomfrancais <- as.character(data$nomfrancais)
+data$nomlatin <- as.character(data$nomlatin)
+data$GlobalID <- as.character(data$GlobalID)
 data$CreationDate <- as.Date(data$CreationDate, format = "%Y/%m/%d %H:%M:%S%z")
 data$Creator <- as.factor(data$Creator)
 data$EditDate <- as.Date(data$EditDate, format = "%Y/%m/%d %H:%M:%S%z")
 data$Editor <- as.factor(data$Editor)
 data$feuillage <- as.factor(data$feuillage)
-data$remarquable <- as.factor(data$remarquable)
 
+data$remarquable <- data$remarquable == "Oui"
+
+
+summary(data$X)
 # Nettoyage des données
 # Valeurs manquantes, valeurs aberrantes et Doublons
 
 
+# met tous les caractères en miniscule
+for (i in 1:ncol(data)) {
+    data[,i] <- tolower(data[,i])
+}
+#supprimer toute les lignes avec des valeurs manquantes en X ou Y
+data <- data[!is.na(data$X) | !is.na(data$Y),]
 
+summary(data$X)
 # Suppression des doublons
 data <- unique(data)
 
@@ -86,11 +90,11 @@ for (i in 1:ncol(data)) {
 
 
 # Affichage des premières lignes du jeu de données après nettoyage
-head(data)
+#head(data)
 
 
 # Histogramme de la hauteur totale
-hist(data$haut_tot, main = "Histogramme de la hauteur totale", xlab = "Hauteur totale", ylab = "Fréquence", col = "lightblue", border = "black")
+#hist(data$haut_tot, main = "Histogramme de la hauteur totale", xlab = "Hauteur totale", ylab = "Fréquence", col = "lightblue", border = "black")
 
 # Fréquence des variables catégorielles
 #cat_vars <- c("X", "Y", "OBJECTID", "created_user", "src_geo", "clc_quartier", "clc_secteur", "id_arbre", "fk_arb_etat", "fk_stadedev", "fk_port", "fk_pied", "fk_situation", "commentaire_environnement", "fk_prec_estim", "clc_nbr_diag", "fk_nomtech", "last_edited_user", "villeca", "nomfrancais", "nomlatin", "GlobalID", "Creator", "Editor", "feuillage", "remarquable")
